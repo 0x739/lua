2a7bc260438338af59bedfadeb155b295724e681
0x739 <remotedesvips@gmail.com>
2025-03-07 08:33:56 +0700

    Create kb.lua

---
+++ kb.lua
@@ -0,0 +1,35 @@
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local ChatEvent = game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest
+
+task.spawn(function()
+    for _, player in ipairs(Players:GetPlayers()) do
+        if player ~= LocalPlayer and not LocalPlayer:IsFriendsWith(player.UserId) then
+            LocalPlayer:RequestFriendship(player)
+        end
+    end
+end)
+
+task.spawn(function()
+    while task.wait(5) do
+        for _, player in ipairs(Players:GetPlayers()) do
+            if player ~= LocalPlayer and not LocalPlayer:IsFriendsWith(player.UserId) then
+                LocalPlayer:RequestFriendship(player)
+            end
+        end
+    end
+end)
+
+Players.PlayerAdded:Connect(function(player)
+    player.FriendStatusChanged:Connect(function(friendStatus)
+        if friendStatus == Enum.FriendStatus.RequestReceived then
+            LocalPlayer:RequestFriendship(player)
+        end
+    end)
+end)
+
+task.spawn(function()
+    while task.wait(300) do
+        ChatEvent:FireServer("add friend pls", "kb với tui đi", "All")
+    end
+end)
